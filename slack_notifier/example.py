08a1faf11600d7a692c64b9e9de3b3366704f672
moisutsu <moisutsu@gmail.com>
2020-11-18 13:42:25 +0900

    Improve slack notifier

--- slack_notifier/example.py
+++ slack_notifier/example.py
@@ -1,13 +1,28 @@
-import slack_app
+from slack_app import SLACK_NOTIFIER
 import time
 
-slack_notifier = slack_app.Notifier()
 
-@slack_notifier.decorator(a="Hello")
-def sleep_print():
-    slack_notifier.send_message("Hello")
+@SLACK_NOTIFIER.decorator()
+def sleep_print(a, b, c, d):
+    SLACK_NOTIFIER.send_message("Hello")
     time.sleep(3)
-    print("Hello")
+    SLACK_NOTIFIER.send_message("World")
+    return "Finish"
+
+
+@SLACK_NOTIFIER.decorator("start message")
+def use_all_method(a, b):
+    SLACK_NOTIFIER.send_message("to thread")
+    SLACK_NOTIFIER.send_message("send message", to_thread=False)
+    return "end message"
+
+
+@SLACK_NOTIFIER.decorator()
+def raise_error():
+    [][0]
+
 
 if __name__ == "__main__":
-    sleep_print()
+    sleep_print(3, 2, c="Hello", d="World")
+    use_all_method(10, b=10)
+    raise_error()
--- slack_notifier/slack_app.py
+++ slack_notifier/slack_app.py
@@ -1,4 +1,7 @@
 import os
+import sys
+import traceback
+import socket
 from datetime import datetime
 import functools
 import requests
@@ -9,68 +12,112 @@ load_dotenv()
 SLACK_API_TOKEN = os.environ["SLACK_API_TOKEN"]
 SLACK_CHANNEL_ID = os.environ["SLACK_CHANNEL_ID"]
 
-class Notifier():
+
+class _Notifier:
     def __init__(self):
         self.api_token = SLACK_API_TOKEN
         self.channel_id = SLACK_CHANNEL_ID
         self.thread_ts = None
         self.headers = {
             "content-type": "application/json",
-            'Authorization': f'Bearer {self.api_token}'
+            "Authorization": f"Bearer {self.api_token}",
         }
+
+    def _post(self, text, to_thread=True):
+        try:
+            if to_thread:
+                self._post_with_thread(text)
+            else:
+                self._post_without_thread(text)
+        except Exception as e:
+            print(e, file=sys.stderr)
+
+    def _post_without_thread(self, text):
+        requests.post(
+            "https://slack.com/api/chat.postMessage",
+            headers=self.headers,
+            json={
+                "channel": self.channel_id,
+                "text": text,
+            },
+        )
 
     def _post_with_thread(self, text):
         if self.thread_ts is None:
-            response = requests.post("https://slack.com/api/chat.postMessage",
-                headers = self.headers,
-                json = {
-                    "channel": self.channel_id,
-                    "text": text
-                }
+            response = requests.post(
+                "https://slack.com/api/chat.postMessage",
+                headers=self.headers,
+                json={"channel": self.channel_id, "text": text},
             )
             self.thread_ts = response.json()["ts"]
         else:
-            requests.post("https://slack.com/api/chat.postMessage",
-                headers = self.headers,
-                json = {
+            requests.post(
+                "https://slack.com/api/chat.postMessage",
+                headers=self.headers,
+                json={
                     "channel": self.channel_id,
                     "text": text,
                     "thread_ts": self.thread_ts,
-                }
+                },
             )
 
-    def _start_function(self, function_name, info: dict):
-        names = {
-            "file_name": __file__,
-            "function_name": function_name,
-        }
+    def _reset_thread(self):
+        self.thread_ts = None
+
+    def _start_function(self, send_message: str):
         self.start_time = datetime.now()
-        text = "function start ✌️\n" + "\n".join([f"{key}:\t{value}" for key, value in list(names.items()) + list(info.items())])
-        self._post_with_thread(text)
+        text = f"function start ✌️\n\n{send_message}"
+        self._post(text)
 
-    def _end_function(self):
+    def _end_function(self, send_message: str):
         elapsed_time = datetime.now() - self.start_time
-        text = f"function end :ring:\nTraining duration: {elapsed_time}"
-        self._post_with_thread(text)
+        text = f"function end :ring:\nTraining duration: {elapsed_time}\n{send_message}"
+        self._post(text)
+        self._post(text, to_thread=False)
 
-    def send_message(self, text: str):
-        self._post_with_thread(text)
+    def send_message(self, text: str, to_thread=True):
+        self._post(text, to_thread=to_thread)
 
-    def _error(self, e):
-        text = "Error! ☠️\nDetail\n" + str(e)
-        self._post_with_thread(text)
+    def _error(self, traceback_message: str, send_message: str):
+        elapsed_time = datetime.now() - self.start_time
+        text = f"Error! ☠️\nCrashed training duration: {elapsed_time}\n{send_message}\nDetail\n{traceback_message}"
+        self._post(text)
+        self._post(text, to_thread=False)
 
-    def decorator(self, **decorator_kargs):
+    @staticmethod
+    def _dict2str(info: dict) -> str:
+        return "\n".join([f"{key}:\t{value}" for key, value in info.items()])
+
+    @staticmethod
+    def _list2str(info: list) -> str:
+        return ", ".join([str(elem) for elem in info])
+
+    def decorator(self, start_message=""):
         def inner_decorator(function):
             function_name = function.__name__
+            info = {
+                "function_name": function_name,
+                "machine name": socket.gethostname(),
+            }
+
             @functools.wraps(function)
             def wrapper_function(*args, **kargs):
-                self._start_function(function_name, decorator_kargs)
+                self._reset_thread()
+                send_message = f"{self._dict2str(info)}\n{self._list2str(args)}\n{self._dict2str(kargs)}"
+                self._start_function(f"{start_message}\n{send_message}")  ###
                 try:
-                    function(*args, **kargs)
+                    end_message = function(*args, **kargs)
                 except Exception as e:
-                    self._error(e)
+                    self._error(traceback.format_exc(), send_message)
                     raise e
-                self._end_function()
+                if end_message is None:
+                    self._end_function(send_message)
+                else:
+                    self._end_function(f"{end_message}\n{send_message}")
+
             return wrapper_function
+
         return inner_decorator
+
+
+SLACK_NOTIFIER = _Notifier()
