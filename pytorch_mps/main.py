cb7c4a54e577a82c224cdcb45089e56513e95259
moisutsu <moisutsu@gmail.com>
2022-06-04 19:04:07 +0900

    Update pytorch_mps

--- pytorch_mps/main.py
+++ pytorch_mps/main.py
@@ -1,15 +1,30 @@
-import timeit
+from time import time
 
 import torch
 
-gpu_device = "mps"
-dtype = torch.float
-number = 10
-size = 8192
-cpumat = torch.randn(size,size,dtype=dtype)
-gpumat = cpumat.to(gpu_device)
 
-cpu_time = timeit.timeit("torch.matmul(cpumat,cpumat)",number=number,globals=globals())
-print("cpu_time: {:.6f} seconds".format(cpu_time))
-gpu_time = timeit.timeit("torch.matmul(gpumat,gpumat)",number=number,globals=globals())
-print("gpu_time: {: .6f} seconds".format(gpu_time))
+def main():
+    device = torch.device("mps")
+
+    print(device)
+
+    size = 8192
+
+    x1 = torch.randn(size, size).to(device)
+    x2 = torch.randn(size, size).to(device)
+
+    start = time()
+    for _ in range(10):
+        x1 @ x2
+    print(f"On GPU: {time() - start:.2f}")
+
+    y1 = torch.randn(size, size)
+    y2 = torch.randn(size, size)
+
+    start = time()
+    for _ in range(10):
+        y1 @ y2
+    print(f"On CPU: {time() - start:.2f}")
+
+if __name__ == "__main__":
+    main()
